04dea979bb701c1c860ff1f2715f7d16b2473831
Joar Heimonen <contact@joar.me>
2025-05-12 14:26:34 +0200

    fix: update

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 podman build -t grafana-netconfaggregator .
-podman run -d -p 3000:3000 --name grafana grafana-netconfaggregator
+podman run -d -p 3000:3000 -p 3001:3001 --name grafana grafana-netconfaggregator
